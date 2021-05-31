9cac015d120ebf86f6f6fbea1b23415cb053b41f
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-05-31 16:35:21 +0530

    feat: turened video card into link

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,27 +1,33 @@
 import "./VideoCard.css";
-const VideoCard = ({props}) => {
-    const { name, thumbnail, views, creator } = props;
-    console.log(props);     
+import { Link } from "react-router-dom";
+
+const VideoCard = ({ props }) => {
+    const { name, thumbnail, views, creator, embedId } = props;
+    console.log(props);
     return (
         <div className="VideoCard">
-            <div className="card-video">
-                <div className="card-video__thumbnail__wrapper">
-                    <img src={thumbnail} alt="" className="card-video__thumbnail" />
-                    <div className="card-video__icon">
-                        <span className="material-icons-round md-36">
-                            watch_later
-                        </span>
+            <Link to={`/videos/${embedId}`}>
+                <div className="card-video">
+                    <div className="card-video__thumbnail__wrapper">
+                        <img
+                            src={thumbnail}
+                            alt=""
+                            className="card-video__thumbnail"
+                        />
+                        <div className="card-video__icon">
+                            <span className="material-icons-round md-36">
+                                watch_later
+                            </span>
+                        </div>
+                    </div>
+                    <div className="video-cart__text__wrapper">
+                        <div className="card-video__title">{name}</div>
+                        <div className="card-video__details">
+                            {creator} • {views} Views
+                        </div>
                     </div>
                 </div>
-                <div className="video-cart__text__wrapper">
-                    <div className="card-video__title">
-                        {name}
-                    </div>
-                    <div className="card-video__details">
-                        { creator } • {views} Views
-                    </div>
-                </div>
-            </div>
+            </Link>
         </div>
     );
 };
--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -1,15 +1,25 @@
-const VideoPage = ({ embedId }) => {
+import "./VideoPage.css"
+import { useParams } from "react-router";
+
+const VideoPage = () => {
+    const { id } = useParams();
     return (
         <div className="video-responsive">
             <iframe
                 width="853"
                 height="480"
-                src={`https://www.youtube.com/embed/${embedId}`}
+                src={`https://www.youtube.com/embed/${id}`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded youtube"
             />
+
+            {/* ! Put Video Details Here */}
+
+            <button>Watch Later</button>
+            <button>Like</button>
+            <button>Share</button>
         </div>
     );
 };
