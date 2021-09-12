87c9057814c6cb35c747f40da3873c260ea4376a
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-09-13 02:08:18 +0530

    styled login page + refactor AuthContext

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
+import "./Login.css";
 import { useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
+import Avatar from "../assets/account_circle_black_48dp.svg";
 
 const Login = () => {
     const { loggedIn, login } = useContext(AuthContext);
@@ -25,41 +28,60 @@ const Login = () => {
         });
     };
     return (
-        <div>
-            <p>login: {loggedIn.toString()}</p>
+        <div className="login">
+            {/* <p>login: {loggedIn.toString()}</p> */}
             <p>{input.username}</p>
             <p>{input.password}</p>
             {loggedIn ? (
-                <>
-                    <p>You are Logged In</p>
-                    <button onClick={login}>Logout</button>
-                </>
+                <div className="logged-in-container">
+                    <img
+                        className="logged-in__image mt-2"
+                        src={Avatar}
+                        alt="Avatar Logo"
+                    />
+                    <div className="heading--h6 mt-1 mb-1">Hi {}!</div>
+                    <button className="btn">Logout</button>
+                </div>
             ) : (
-                <form onSubmit={handleSubmit}>
-                    <div className="input-text-wrapper">
-                        <input
-                            className="input-text input-text-email"
-                            type="text"
-                            placeholder="Email"
-                            value={input.username}
-                            name="username"
-                            onChange={handleChange}
-                        />
+                <div className="login-form-container">
+                    <div className="heading heading--h4 login-heading">
+                        Welcome Back!
                     </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="input-text-wrapper mb-1">
+                            <input
+                                className="input-text input-text-email"
+                                type="text"
+                                placeholder="Email"
+                                value={input.username}
+                                name="username"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                    <div class="input-text-wrapper">
-                        <input
-                            class="input-text input-text-password"
-                            type="password"
-                            placeholder="Password"
-                            value={input.password}
-                            name="password"
-                            onChange={handleChange}
-                        />
-                    </div>
+                        <div class="input-text-wrapper mb-1">
+                            <input
+                                class="input-text input-text-password"
+                                type="password"
+                                placeholder="Password"
+                                value={input.password}
+                                name="password"
+                                onChange={handleChange}
+                            />
+                        </div>
 
-                    <button class="btn btn--lg">LOGIN</button>
-                </form>
+                        <button class="btn btn--md login-btn mt-1 mb-1">
+                            LOGIN
+                        </button>
+
+                        <Link
+                            className="btn btn--link login-btn-link"
+                            to="/signup"
+                        >
+                            Don't Have Account Create One!
+                        </Link>
+                    </form>
+                </div>
             )}
         </div>
     );
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,33 +1,104 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import fakeAPI from "../api/fakeAPI";
 
 export const AuthContext = createContext();
 
+const emptyUser = {
+    userId: "",
+    name: "",
+    email: "",
+    token: "",
+};
 export const AuthProvider = ({ children }) => {
-    const [loggedIn, setLoggedIn] = useState(false);
+    const { isUserLogin: initialLogin, userDetails: initialUserDetails } =
+        JSON.parse(localStorage.getItem("login")) || {
+            isUserLogin: false,
+            userDetails: emptyUser,
+        };
 
-    const navigate = useNavigate();
+    const [isUserLogin, setLogin] = useState(initialLogin);
+    const [userDetails, setUserDetails] = useState(initialUserDetails);
     const { state } = useLocation();
+    const navigate = useNavigate();
 
-    const login = async (username, password) => {
-        if (loggedIn) {
-            setLoggedIn(false);
+    const loginWithCredential = async (email, password) => {
+        if (isUserLogin) {
+            logout();
         } else {
             try {
-                const response = await fakeAPI(username, password);
-                if (response.success) {
-                    setLoggedIn(true);
+                const response = await axios.post("/users/login", {
+                    email,
+                    password,
+                });
+                if (response.data.success) {
+                    console.log({ ...response.data });
+                    console.log(response.data.name);
+                    setLogin(true);
+                    setUserDetails({
+                        userId: response.data.id,
+                        name: response.data.name,
+                        email: response.data.email,
+                        token: response.data.token,
+                    });
+                    console.log("use", userDetails.userId);
+                    console.log("use", userDetails);
+                    localStorage?.setItem(
+                        "login",
+                        JSON.stringify({
+                            isUserLogin: true,
+                            userDetails: {
+                                userId: response.data.id,
+                                name: response.data.name,
+                                email: response.data.email,
+                                token: response.data.token,
+                            },
+                        })
+                    );
                 }
                 navigate(state?.from ? state.from : "/");
-            } catch (err) {
-                console.log("Something is wrong");
+            } catch (error) {
+                console.log("Something Went Wrong!", error);
             }
         }
     };
 
+    const logout = () => {
+        console.log("Logged Out!");
+        setLogin(false);
+        setUserDetails(emptyUser);
+        localStorage.removeItem("login");
+    };
+
+    const signUp = async (userDetail) => {
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/users/signup",
+                {
+                    ...userDetail,
+                }
+            );
+            if (response.data.success) {
+                console.log("New User Created!");
+                // ! USER NEED TO BE LOGGED IN HERE!
+                navigate("/");
+            }
+        } catch (error) {
+            console.log("Error While Creating New User!", error);
+        }
+    };
     return (
-        <AuthContext.Provider value={{ loggedIn, login }}>
+        <AuthContext.Provider
+            value={{
+                isUserLogin,
+                loginWithCredential,
+                userId: userDetails.userId,
+                token: userDetails.token,
+                name: userDetails.name,
+                logout,
+                signUp,
+            }}
+        >
             {children}
         </AuthContext.Provider>
     );
