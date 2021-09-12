00f258af8ed05737cbb11437de03db2485a5aa48
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-09-13 01:34:05 +0530

    style: styled nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,32 +5,34 @@ const Nav = () => {
     return (
         <div className="nav">
             <Link to="/">
-                <div className="nav__brand">Sinēmā</div>
+                <div className="nav__logo">Sinēmā</div>
             </Link>
 
-            <Link to="/liked">
-                <div class="icon-with-badge">
-                    <span class="material-icons-outlined icon-with-badge__icon">
-                        favorite
-                    </span>
-                </div>
-            </Link>
+            <div className="nav__menu">
+                <Link className="nav__item" to="/liked">
+                    <div class="icon-with-badge">
+                        <span class="material-icons-outlined icon-with-badge__icon">
+                            favorite
+                        </span>
+                    </div>
+                </Link>
 
-            <Link to="/later">
-                <div class="icon-with-badge">
-                    <span class="material-icons-outlined icon-with-badge__icon">
-                        bookmark
-                    </span>
-                </div>
-            </Link>
+                <Link className="nav__item" to="/later">
+                    <div class="icon-with-badge">
+                        <span class="material-icons-outlined icon-with-badge__icon">
+                            bookmark
+                        </span>
+                    </div>
+                </Link>
 
-            <Link to="/login">
-                <div class="icon-with-badge">
-                    <span class="material-icons icon-with-badge__icon">
-                        person
-                    </span>
-                </div>
-            </Link>
+                <Link className="nav__item" to="/login">
+                    <div class="icon-with-badge">
+                        <span class="material-icons icon-with-badge__icon">
+                            person
+                        </span>
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 };
