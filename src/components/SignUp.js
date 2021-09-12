65384949fb8d8b8d473936d66f7e9b27097e8afb
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-09-13 02:22:14 +0530

    feat: added sign up page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,76 @@
+import "./SignUp.css";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
+const initialUserDetails = {
+    name: "",
+    email: "",
+    password: "",
+};
+
 const SignUp = () => {
-    return <div>Sign Up Page</div>
+    const [userDetails, setUserDetails] = useState(initialUserDetails);
+    const { signUp } = useContext(AuthContext);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        signUp(userDetails);
+        setUserDetails(initialUserDetails);
+    };
+
+    const handleChange = (e) => {
+        setUserDetails((userDetails) => ({
+            ...userDetails,
+            [e.target.name]: e.target.value,
+        }));
+    };
+    return (
+        <div className="signup">
+            <div className="sign-up-from-container">
+                <div className="heading heading--h4 sign-up-heading">
+                    Create Account!
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <div class="input-text-wrapper mb-1">
+                        <input
+                            class="input-text  input-text-full-name"
+                            type="text"
+                            placeholder="Full Name"
+                            name="name"
+                            value={userDetails.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div class="input-text-wrapper mb-1">
+                        <input
+                            class="input-text input-text-email"
+                            type="text"
+                            placeholder="Email"
+                            name="email"
+                            value={userDetails.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div class="input-text-wrapper">
+                        <input
+                            class="input-text input-text-password"
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={userDetails.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <button class="btn btn--lg sign-up-btn mt-1">
+                        SIGN UP
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default SignUp;
