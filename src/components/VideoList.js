0e7c1ac534d6a4ad13476d076b91d6365edafaea
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2021-10-08 00:31:09 +0530

    style: styled VideoList Component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,3 +1,4 @@
+import "./VideoList.css";
 import { useContext } from "react";
 import { DataContext } from "../contexts/DataContext";
 import VideoCard from "./VideoCard";
@@ -5,12 +6,14 @@ import VideoCard from "./VideoCard";
 const VideoList = () => {
     const { video } = useContext(DataContext);
     return (
-        <div>
-            {video[0] ? (
-                video.map((video) => <VideoCard props={video} />)
-            ) : (
-                <div>Loading</div>
-            )}
+        <div className="video-list-container">
+            <div className="video-list">
+                {video[0] ? (
+                    video.map((video) => <VideoCard props={video} />)
+                ) : (
+                    <div>Loading</div>
+                )}
+            </div>
         </div>
     );
 };
