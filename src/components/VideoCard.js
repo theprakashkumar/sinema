ec996d136aeab45a463cd8f3bb888dcf51ec2edc
PRAKASH <maiprakashkumar@gmail.com>
2022-03-19 17:17:10 +0530

    refactor: remove clock icon

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -14,11 +14,6 @@ const VideoCard = ({ props }) => {
                             alt=""
                             className="card-video__thumbnail"
                         />
-                        <div className="card-video__icon">
-                            <span className="material-icons-round md-36">
-                                watch_later
-                            </span>
-                        </div>
                     </div>
                     <div className="video-cart__text__wrapper">
                         <div className="card-video__title">{name}</div>
